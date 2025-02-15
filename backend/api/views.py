8d70474bbf0ce07d2dd80f713975f950918a891b
Artem Selivanov <mr.anderson203033@gmail.com>
2025-02-15 03:06:08 +0300

    Add Actions 5

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+
 from djoser import views
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
