b311d650aa224a48eb30307459de788cc6e4f9ea
Artem Selivanov <mr.anderson203033@gmail.com>
2025-02-15 17:18:03 +0300

    Add Actions 12

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser, get_user_model
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
