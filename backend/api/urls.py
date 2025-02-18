893cde037040144254f76cb1a78abcba5cec5450
Artem Selivanov <mr.anderson203033@gmail.com>
2025-02-18 13:32:57 +0300

    Add Actions 23

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from djoser.views import TokenCreateView, TokenDestroyView
 from rest_framework.routers import DefaultRouter
 
 from api.views import (
@@ -16,11 +15,7 @@ router.register('users', UserListViewSet, basename='user')
 
 
 urlpatterns = [
-    path('auth/token/login/', TokenCreateView.as_view(), name='token_login'),
-    path(
-        'auth/token/logout/', TokenDestroyView.as_view(), name='token_logout'
-    ),
     path('', include(router.urls)),
-    path('api/', include('djoser.urls')),
-    path('api/auth/', include('djoser.urls.authtoken'))
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken'))
 ]
