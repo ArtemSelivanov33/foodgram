ca1b30beaa078d2232c79eec6526258723ec4d18
Artem Selivanov <mr.anderson203033@gmail.com>
2025-03-21 11:37:12 +0300

    Add Actions 51

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,3 +1,4 @@
+import os
 from hashlib import blake2b
 
 from django.conf import settings
@@ -6,6 +7,12 @@ from foodgram_backend.constants import DIGEST_SIZE
 
 
 def generate_short_url(url):
-    base_url = f'https://{settings.ALLOWED_HOSTS[0]}/s/'
+    domain = os.getenv('DOMAIN', 'localhost')
+    base_url = f'https://{domain}/'
     hash_url = blake2b(url.encode(), digest_size=DIGEST_SIZE).hexdigest()
     return f'{base_url}{hash_url}'
+
+# def generate_short_url(request):
+#     """Метод для получения URL текущей страницы."""
+#     current_url = request.build_absolute_uri()
+#     return current_url
