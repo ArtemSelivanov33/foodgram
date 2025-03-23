a539b29f3d0bbd77014fe4a858c8e197387ca2bd
Artem Selivanov <mr.anderson203033@gmail.com>
2025-03-23 20:33:24 +0300

    Add Actions 61

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,4 @@
 import os
-# from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,6 +6,7 @@ from foodgram_backend.constants import ADDITIONAL_USER_FIELDS
 
 User = get_user_model()
 
+
 @admin.register(User)
 class AdminUser(BaseUserAdmin):
     add_fieldsets = BaseUserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
