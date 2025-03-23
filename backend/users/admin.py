c27e15c1eb390a04ec3fe3d93a86f5883bb2ee86
Artem Selivanov <mr.anderson203033@gmail.com>
2025-03-23 20:21:13 +0300

    Add Actions 60

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
 
 from foodgram_backend.constants import ADDITIONAL_USER_FIELDS
-from users.models import User
 
+User = get_user_model()
 
 @admin.register(User)
 class AdminUser(BaseUserAdmin):
-    model = User
-
     add_fieldsets = BaseUserAdmin.add_fieldsets + ADDITIONAL_USER_FIELDS
     fieldsets = BaseUserAdmin.fieldsets + ADDITIONAL_USER_FIELDS
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from foodgram_backend import constants
@@ -24,15 +25,26 @@ class User(AbstractUser):
         null=True,
         upload_to='avatars'
     )
-    is_subscribed = models.BooleanField(
+    is_subscribed = models.ManyToManyField(
+        to='self',
         verbose_name='Подписка',
-        default=False
+        related_name='subscribers',
+        symmetrical=False,
+        blank=True,
     )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
         ordering = ('username',)
+
+    def clean(self):
+        super().clean()
+        if self.username.lower() == 'me':
+            raise ValidationError('Имя пользователя не может быть "me".')
 
     def __str__(self):
         return self.username[:constants.USERNAME_CUT]
