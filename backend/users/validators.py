ac9349811a0e080339461aed932d36a4f2d0308c
Artem Selivanov <mr.anderson203033@gmail.com>
2025-03-31 04:17:33 +0300

    Add Actions 134

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 
 
-def validate_username(value):
+def validate_username_me(value):
     if value.lower() == 'me':
         raise ValidationError(
             ('Пользователь с именем "me" не может зарегистрироваться.'),
