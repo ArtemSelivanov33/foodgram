06aa2821b6eca2aed395a1ee941d73cbf2d9d806
Artem Selivanov <mr.anderson203033@gmail.com>
2025-04-11 10:53:41 +0300

    Add Actions 178

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -14,6 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
     is_subscribed = serializers.SerializerMethodField(
         read_only=True,
     )
+    image = serializers.ImageField()
 
     class Meta:
         model = User
